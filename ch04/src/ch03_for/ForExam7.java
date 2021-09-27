65a4b901d5b7671a968cbe69c24ee21f6d33e420
Hey <Hey@DESKTOP-NQSEM0J>
2021-09-27 23:31:25 +0900

    21.9.27

--- ch04/src/ch03_for/ForExam7.java
+++ ch04/src/ch03_for/ForExam7.java
@@ -4,14 +4,12 @@ public class ForExam7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//초기화 부분이나 증감부분에 ,로 명령문을 나열할 수 있음.
-	for(int i=1 , y=1; i<10; i++ , System.out.println(y)) {//
-		System.out.println("hello");
-		//for블럭을 빠져나가나는 조건을 블럭내에서 처리{}
-		if(i==10) break;//블럭을 빠져나가는 명령문 break;
-		y++;
+      //for(초기화;for블럭을 빠져나가는 조건;증감){실행문;}
+	  for(int i=1;  ;i++) {//for 블럭을 빠져나가는 조건이 없는 경우, 조건을 true로 간주.
+		  System.out.println(i+"hello");
+		  //for블럭을 빠져나가는 조건을 블럭내에서 처리{}
+		  if(i==10) break;//블럭을 빠져나가는 명령문 break;
+	  }
+	     System.out.println("종료");
 	}
-		System.out.println("종료");
-	}
-
 }
